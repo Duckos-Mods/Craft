e8ba50a411ec9c9c34a5f4181bf94c398c845bea
Duckos-Mods <businwes@hotmail.com>
2024-06-03 02:43:23 +0100

    changed Deps

--- xmake.lua
+++ xmake.lua
@@ -1,6 +1,5 @@
 add_rules("mode.debug", "mode.release")
-add_requires("llvm-prebuilt")
-add_requires("zasm")
+add_requires("llvm-prebuilt 16.0.6")
 
 set_languages("c++23")
 target("Craft")
@@ -9,10 +8,12 @@ target("Craft")
     add_includedirs("src", {public = true})
     add_headerfiles("src/**.hpp")
     add_packages("llvm-prebuilt", {public = true})
-    add_packages("zasm", {public = true})
+    set_runtimes("MD")
+
 
 target("CraftTests")
     set_kind("binary")
     add_files("test/**.cpp")
     add_headerfiles("test/**.hpp")
+    set_runtimes("MD")
     add_deps("Craft")
