634e3459f0e5dba03bcfd25d7a49e248b04aabdf
Duckos-Mods <businwes@hotmail.com>
2024-06-05 13:32:52 +0100

    added some config stuff and zydis

--- xmake.lua
+++ xmake.lua
@@ -1,5 +1,24 @@
 add_rules("mode.debug", "mode.release")
 add_requires("llvm-prebuilt 16.0.6")
+add_requires("zydis")
+
+MSVC_COMPILE_OPTIONS = {
+    ["ldflags"] = {
+        "/DEBUG:FULL"
+    },
+    ["shflags"] = {
+        "/DEBUG:FULL"
+    }
+}
+
+local function apply_options_to_target(target)
+    for option, values in pairs(MSVC_COMPILE_OPTIONS) do
+        target:add(option, values, { tools = {"clang_cl", "cl", "link"}})
+        print("Added option: " .. option .. " with values: " .. table.concat(values, ", "))
+    end  
+
+end
+
 
 set_languages("c++23")
 target("Craft")
@@ -8,8 +27,13 @@ target("Craft")
     add_includedirs("src", {public = true})
     add_headerfiles("src/**.hpp")
     add_packages("llvm-prebuilt", {public = true})
+    add_packages("zydis", {public = false})
     set_runtimes("MD")
-
+    on_config(function(target)
+        print("Configuring Craft")
+        apply_options_to_target(target)
+    end)
+    set_symbols("debug")
 
 target("CraftTests")
     set_kind("binary")
@@ -17,3 +41,8 @@ target("CraftTests")
     add_headerfiles("test/**.hpp")
     set_runtimes("MD")
     add_deps("Craft")
+    on_config(function(target)
+        print("Configuring CraftTests")
+        apply_options_to_target(target)
+    end)
+    set_symbols("debug")
