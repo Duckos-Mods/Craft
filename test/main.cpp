4a82611521568ce4911bbdbad3bbaf2e1f8686b7
Duckos-Mods <businwes@hotmail.com>
2024-06-05 13:37:01 +0100

    Testing codes

--- test/main.cpp
+++ test/main.cpp
@@ -3,24 +3,43 @@
 #include <map>
 #include <list>
 #include <print>
+#include <chrono>
+#include <intrin.h>
 
-
-struct alignas(16) thing
-{
-    float x, y, z, w, f;
-};
 
 [[nodiscard]]
-static int HookTarget(float, int, int, int, uint64_t, thing*)
+int HookTarget()
 {
-	return 0;
+    std::println("CALLED HookTarget");
+    return 69;
+}
+
+void* ogFunc = nullptr;
+
+
+[[nodiscard]]
+static int ReplaceHookTarget()
+{
+    return reinterpret_cast<int(*)()>(ogFunc)();
 }
 
 [[nodiscard]]
-static void PreHook(float& i, int& j, int& k, int& l, int& m, thing* t)
+static void PreHook()
 {
-
-
+    std::println("PREHOOK");
+}
+[[nodiscard]]
+static void PostHook(int& returnData, float& i, int& j, int& k, int& l, uint64_t& m, int& n)
+{
+    std::println("Return Data: {}", returnData);
+    returnData++;
+    i++;
+    j++;
+    k++;
+    l++;
+    m++;
+    n++;
+    std::println("i: {}, j: {}, k: {}, l: {}, m: {}, n: {}", i, j, k, l, m, n);
 }
 
 int main(int argc, char** argv) {
@@ -28,18 +47,25 @@ int main(int argc, char** argv) {
     Craft::CraftContext ctx(argc, argv);
 
     Craft::ManagerHook hm;
-    auto returnTI = Craft::GetTypeInformation<void>();
-    auto arg1TI = Craft::GetTypeInformation<int>();
-    auto arg2TI = Craft::GetTypeInformation<thing*>();
-    auto arg3TI = Craft::GetTypeInformation<thing*>();
-    auto arg4TI = Craft::GetTypeInformation<thing*>();
-    auto arg5TI = Craft::GetTypeInformation<thing*>();
-    auto arg6TI = Craft::GetTypeInformation<int>();
-    std::vector<Craft::TypeInformation> argTIs = {arg1TI, arg2TI, arg3TI, arg4TI, arg5TI, arg6TI};
-    /*for (int i = 0; i < 20; i++)
-        argTIs.push_back(Craft::GetTypeInformation<int>());*/
+    auto returnTI = Craft::GetTypeInformation<int>();
+    std::vector<Craft::TypeInformation> argTIs;
+    
     auto NHI = Craft::GetNeededHookInfo(returnTI, argTIs);
+    // Start timer
+    auto start = std::chrono::high_resolution_clock::now();
+    auto Func = Craft::NonStaticLocalFunctionToRealAddress(HookTarget);
+    hm.CreateManagerHook(Func, PreHook, NHI, Craft::HookType::PreHook, &ctx, true);
+    // Stop timer
+    auto stop = std::chrono::high_resolution_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
+    std::println("Time taken to create hook: {} microseconds", duration.count());
 
-    hm.CreateManagerHook(HookTarget, PreHook, NHI, Craft::HookType::PreHook, false);
+    //hm.AddNewFuncToHooksWithNoProcess(PreHook);
+    //hm.AddPreNewFuncToHooksWithNoProcess(PostHook);
+    //hm.WriteReplaceBypass(ReplaceHookTarget);
+
+    ogFunc = hm.GetOriginalFunc();
+    HookTarget();
+
 }
 
