eeb3466ce6f7d2badfb21618984c34f381e20e1f
Duckos-Mods <businwes@hotmail.com>
2024-06-05 13:33:39 +0100

    Pre cache some strings

--- src/Internal/CraftContext.cpp
+++ src/Internal/CraftContext.cpp
@@ -4,7 +4,13 @@
 #include <llvm/Support/TargetSelect.h>
 #include <llvm/Support/raw_ostream.h>
     
-#include <llvm/Support/FileSystem.h>
+#include <llvm/Support/TargetSelect.h>
+#include <llvm/Support/AllocatorBase.h>
+#include <llvm/TargetParser/Host.h>
+#include <llvm/ADT/StringMap.h>
+#include <sstream>
+	
+
 namespace Craft
 {
 	CraftContext::CraftContext(int argc, char** argv) : mllvmInit(argc, argv, true)
@@ -18,6 +24,22 @@ namespace Craft
         llvm::InitializeNativeTarget();
         llvm::InitializeNativeTargetAsmParser();
         llvm::InitializeNativeTargetAsmPrinter();
+
+		llvm::StringMap<bool, llvm::MallocAllocator> features;
+		llvm::sys::getHostCPUFeatures(features);
+        std::stringstream ss;
+		for (auto& [key, value] : features)
+		{
+            if (!value)
+                continue;
+            ss << std::format("+{},", key.str());
+            mCPUFeaturesVec.push_back(std::format("+{}", key.str()));
+		}
+        // Remove trailing comma
+        this->mCpuFeats = ss.str();
+        mCpuFeats.pop_back();
+
+
 	}
     CraftContext::~CraftContext()
     {
--- src/Internal/CraftContext.hpp
+++ src/Internal/CraftContext.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <llvm/Support/InitLLVM.h>
-
-
+#include <string>
+#include <llvm/ADT/StringRef.h>
+#include <vector>
 
 namespace Craft
 {
@@ -10,7 +11,12 @@ namespace Craft
 	public:
 		CraftContext(int argc = 0, char** argv = nullptr);
 		~CraftContext();
+
+		std::string& GetCpuFeats() { return mCpuFeats; }
+		const std::vector<std::string>& GetCPUFeaturesVec() const { return mCPUFeaturesVec; }
 	private:
 		llvm::InitLLVM mllvmInit;
+		std::string mCpuFeats;
+		std::vector<std::string> mCPUFeaturesVec;
 	};
 }
